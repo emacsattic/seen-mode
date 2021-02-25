ae0882e3220facc8c1583d4c33dbbb534b7459ff
Filipe da Silva Santos <contact@shiori.com.br>
2021-02-25 01:58:49 -0300

    Initial, and useless, version

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Table of Contents
+
+
+
+A simple  syntax-highlighting mode for text/kepago
+                                 \_ 
+ <span class="underline">\_</span> <span class="underline">\_</span>  <span class="underline">\_</span>  \_ \_          <span class="underline">\_</span> | |
+(<span class="underline">-// -</span>)/ -<span class="underline">)| ' \\   \_    / -</span>)| |
+*\_\_*\\\_\_\_|\\\_\_\_||\_||\_| (\_)   \\\_\_\_||\_|
+
---
+++ seen-mode.el
@@ -0,0 +1,30 @@
+;;; seen.el --- A syntax highlighting package for text/kepago -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 Filipe da Silva Santos
+
+;; Author: Filipe da Silva Santos <contact@shiori.com.br>
+;; Created: 25 Feb 2021
+;; Keywords: languages
+;; Homepage: https://git.sr.ht/~shiorid/seen.el
+;; Package-Requires: ((emacs "25.1"))
+
+;;; Commentary:
+
+;; This package provides a major mode for editing text/kepago files.
+;; I hope it helps future VN translators and developers.
+
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
